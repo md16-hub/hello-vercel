133d2d5fe645740794cd9f3a90333c6be53a2174
md-hub <md4293@columbia.edu>
2026-02-09 13:16:58 -0500

    Add Hello World page

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+      <main style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontSize: '3rem'
+      }}>
+        Hello World
+      </main>
+  )
+}
